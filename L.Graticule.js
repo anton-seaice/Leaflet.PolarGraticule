06bce9923f9728b9834cc7a6a91da8cd6ff29da2
Anton Steketee <anton.steketee@aad.gov.au>
2022-12-12 12:01:36 +1100

    Update for new versions, improved display on poles, labels, and examples

--- L.Graticule.js
+++ L.Graticule.js
@@ -8,51 +8,86 @@ L.Graticule = L.GeoJSON.extend({
             color: '#333',
             weight: 1
         },
-        interval: 20
+        intervalLat: 10 ,
+        intervalLng: 10 ,
+        latBounds:[-90,90],
+        lngBounds:[-180,180] ,
+        centerLatLabels: true ,
+        centerLonLabels: false ,
+        zIndex: 99999,
+        onEachFeature: function (feature, layer) {
+			if (feature.properties.name.match('W')) {
+				layer.setText(
+					feature.properties.name,
+					{offset:-3, orientation:0, center: layer.options.centerLonLabels}
+				);
+			} else if (feature.properties.name.match('E')) {
+                layer.setText(
+					feature.properties.name,
+					{offset:-3, orientation:180, center: layer.options.centerLonLabels}
+				);
+            }
+            else if (feature.properties.name.match('S')) {
+				layer.setText(
+					feature.properties.name,
+					{offset:-3, center: layer.options.centerLatLabels}
+				);
+			} else if (feature.properties.name.match('N')) {
+				layer.setText(
+					feature.properties.name,
+					{offset:-3, center: layer.options.centerLatLabels}
+				);
+			}
+        }   
+
     },
 
     initialize: function (options) {
+
+        if (options.onEachFeature) {
+            this.options.onEachFeature = null ;
+        }
+
         L.Util.setOptions(this, options);
+        this.options.style={...this.options.style, interactive:false}
         this._layers = {};
 
-        if (this.options.sphere) {
-            this.addData(this._getFrame());
-        } else {
-            this.addData(this._getGraticule());
-        }
-    },
-
-    _getFrame: function() {
-        return { "type": "Polygon",
-          "coordinates": [
-              this._getMeridian(-180).concat(this._getMeridian(180).reverse())
-          ]
-        };
+        this.addData(this._getGraticule());
     },
 
     _getGraticule: function () {
-        var features = [], interval = this.options.interval;
+        var features = []; 
+
+        //round the starting point to a multiple of the lng interval
+        let lng = this.options.intervalLng*Math.round(this.options.lngBounds[0]/this.options.intervalLng)
 
         // Meridians
-        for (var lng = 0; lng <= 180; lng = lng + interval) {
-            features.push(this._getFeature(this._getMeridian(lng), {
-                "name": (lng) ? lng.toString() + "° E" : "Prime meridian"
-            }));
-            if (lng !== 0) {
-                features.push(this._getFeature(this._getMeridian(-lng), {
-                    "name": lng.toString() + "° W"
+        for (lng ; lng < this.options.lngBounds[1]; lng += this.options.intervalLng) {
+            if (lng >=0) {
+                features.push(this._getFeature(this._getMeridian(lng), {
+                "name": (lng) ? lng.toString() + "° E" : "0° E"
+                })); 
+            }
+            else if (lng < 0) {
+                features.push(this._getFeature(this._getMeridian(lng), {
+                    "name": (-lng).toString() + "° W"
                 }));
             }
         }
 
+        //round the starting point to a multiple of the lat interval
+        let lat = this.options.intervalLat*Math.round(this.options.latBounds[0]/this.options.intervalLat)
+
         // Parallels
-        for (var lat = 0; lat <= 90; lat = lat + interval) {
-            features.push(this._getFeature(this._getParallel(lat), {
-                "name": (lat) ? lat.toString() + "° N" : "Equator"
-            }));
-            if (lat !== 0) {
-                features.push(this._getFeature(this._getParallel(-lat), {
-                    "name": lat.toString() + "° S"
+        for (lat; lat < this.options.latBounds[1]; lat = lat + this.options.intervalLat) {
+            if (lat>=0) {
+                features.push(this._getFeature(this._getParallel(lat), {
+                    "name": (lat) ? lat.toString() + "° N" : "0° N"
+                }));
+            }
+            else if (lat < 0) {
+                features.push(this._getFeature(this._getParallel(lat), {
+                    "name": (-lat).toString() + "° S"
                 }));
             }
         }
@@ -66,15 +101,25 @@ L.Graticule = L.GeoJSON.extend({
     _getMeridian: function (lng) {
         lng = this._lngFix(lng);
         var coords = [];
-        for (var lat = -90; lat <= 90; lat++) {
-            coords.push([lng, lat]);
+
+        // for convenience with labelling, we are going to draw lines towards the poles
+        if (this.options.latBounds[0]<-this.options.latBounds[1]) {
+            // lines start at max and go towards the min (e.g.. from -30 towards -90)
+            for (var lat = this.options.latBounds[1]; lat >= this.options.latBounds[0]; lat--) {
+                coords.push([lng, lat]);
+            }
+        } else {
+            // lines start at min and go towards max (e.g. from 0 towards 90)
+            for (var lat = this.options.latBounds[0]; lat <= this.options.latBounds[1]; lat++) {
+                coords.push([lng, lat]);
+            }
         }
         return coords;
     },
 
     _getParallel: function (lat) {
         var coords = [];
-        for (var lng = -180; lng <= 180; lng++) {
+        for (var lng = this.options.lngBounds[0]; lng <= this.options.lngBounds[1]; lng++) {
             coords.push([this._lngFix(lng), lat]);
         }
         return coords;
@@ -95,7 +140,7 @@ L.Graticule = L.GeoJSON.extend({
         if (lng >= 180) return 179.999999;
         if (lng <= -180) return -179.999999;
         return lng;
-    }
+    },
 
 });
 
--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 Leaflet.Graticule
 =================
 
-Generate a graticule grid of latitude and longitude lines.
+Generate a graticule grid of latitude and longitude lines. The graticule inclues labels by default, and support for maps in polar projections.
 
-## API
+<a href="http://blog.thematicmapping.org/2013/07/creating-graticule-with-leaflet.html">This is heavily based on the original L.Graticule plugin</a> and labels are provided using the 
+
+## Usage
 
 ### `L.graticule(options)`
 
-Create a new Graticule layer which inherits from `L.GeoJSON`. Options is an
-optional options object with the following properties:
+Create a new Graticule layer which inherits from `L.GeoJSON`. The following options have been added:
 
-* `interval`: the gap between each graticule line, in degrees
+* `intervalLat`: the gap between each line of latitude, in degrees
+* `intervalLon`: the gap between each line of longtitude, in degrees
+* `latBounds`: the latitudes to draw the graticule over (default: [-90,90])
+* `lngBounds`: the longitudes to draw the graitcule over (default: [-180,180])
+* `centerLatLabels`: center labels along the graticule (default: true) 
+* `centerLngLabels`: center labels along the graticule (default: false) 
 * `style`: [path options](http://leafletjs.com/reference.html#path-options) for
   the generated lines
 
@@ -22,7 +28,7 @@ optional options object with the following properties:
 L.graticule().addTo(map);
 
 // Specify divisions every 10 degrees
-L.graticule({ interval: 10 }).addTo(map);
+L.graticule({ intervalLat: 10, intervalLng: 10 }).addTo(map);
 
 // Specify bold red lines instead of thin grey lines
 L.graticule({
@@ -33,4 +39,7 @@ L.graticule({
 }).addTo(map);
 ```
 
-<a href="http://blog.thematicmapping.org/2013/07/creating-graticule-with-leaflet.html">More information</a>
+## Advanced Use
+
+If you don't like how the labels are displayed, you can specify a custom display function.
+
